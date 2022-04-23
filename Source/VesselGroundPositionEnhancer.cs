90284753ebc387bcf0e9859a41f3b8dc9b1eac95
siimav <siim.aaver@gmail.com>
2022-04-24 00:50:48 +0300

    Add VesselGroundPositionEnhancer

---
+++ Source/VesselGroundPositionEnhancer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using static Vessel;
+
+namespace RealSolarSystem
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class VesselGroundPositionEnhancer : MonoBehaviour
+    {
+        public void Start()
+        {
+            GameEvents.onVesselGoOffRails.Add(OnVesselGoOffRails);
+        }
+
+        public void OnDestroy()
+        {
+            GameEvents.onVesselGoOffRails.Remove(OnVesselGoOffRails);
+        }
+
+        private void OnVesselGoOffRails(Vessel v)
+        {
+            if (v.Landed && !v.Splashed && v.situation != Situations.PRELAUNCH)
+            {
+                v.skipGroundPositioning = false;
+                var sw = System.Diagnostics.Stopwatch.StartNew();
+                v.CheckGroundCollision();
+                Debug.Log($"[RSS-VGPE] CheckGroundCollision() in {sw.ElapsedMilliseconds}ms");
+                sw.Stop();
+            }
+            else
+            {
+                Debug.Log("[RSS-VGPE] not landed or is prelaunch");
+            }
+        }
+    }
+}
